203900674b757b898cfc6b5a6147b934faef6676
TimPlay1 <97138350+TimPlay1@users.noreply.github.com>
2026-01-16 20:58:41 +0300

    Add bootstrap loaders (plain and obfuscated)

---
+++ farm_bootstrap.lua
@@ -0,0 +1,5 @@
+--[[
+    ODY.FARM Farm Loader Bootstrap  
+    Loads the farm script loader from ody.farm
+]]
+loadstring(game:HttpGet("https://scripts.ody.farm/farm_loader.lua"))()
---
+++ main_bootstrap.lua
@@ -0,0 +1,5 @@
+--[[
+    ODY.FARM Main Loader Bootstrap
+    Loads the main script loader from ody.farm
+]]
+loadstring(game:HttpGet("https://scripts.ody.farm/loader.lua"))()
